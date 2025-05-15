1f1dd0cd75fee9d72f30aa629447c8e7750a34c4
shtakasu <kasutaro1793@gmail.com>
2025-05-15 14:47:45 +0900

    update figure number

--- fig3.py
+++ fig3.py
@@ -1,97 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-from utils.util import *
-from scipy import special
-from scipy.stats import pearsonr
 
-n_data = 100
-device = "cpu"
+d_alpha = 0.001
+alpha = np.arange(0.001, 1.001, d_alpha)
+sigma_s_tilde_list = np.arange(0.4, 1.1, 0.1)
+g = 1.2
+sigma_n = 0.5
 
-N = 1000
-Tobs = 50000
-max_leadout = 100
-trial = 10
 
-if device=="cpu":
-    act_func = np.tanh
-elif device=="mps":
-    act_func = torch.tanh
+r_list = []
+r_nondiverge_list = []
+threshold_list = []
 
-min_g = 0.2; max_g = 2.
-min_sigma_n = 0. ; max_sigma_n = 3.
-min_sigma_s = 0.1; max_sigma_s = 3.
+fig = plt.figure(figsize=(7,4))
 
-Lhalf_list = np.zeros(n_data)
-rho_list = np.zeros(n_data)
-g_list = np.zeros(n_data)
-sigma_s_list = np.zeros(n_data)
-sigma_n_list = np.zeros(n_data)
-
-ratio_list = []
-
-for i in tqdm(range(n_data)):
-    g = min_g + (max_g-min_g)*np.random.rand()
-    sigma_n = min_sigma_n + (max_sigma_n-min_sigma_n)*np.random.rand()    
-    sigma_s = min_sigma_s + (max_sigma_s-min_sigma_s)*np.random.rand()    
-            
-    g_list[i] = g; sigma_s_list[i] = sigma_s; sigma_n_list[i] = sigma_n
-    
-    mc_mean, _ = mc_calc(g,N,act_func,max_leadout,sigma_s, sigma_noise=sigma_n, trial=trial, Tobs= Tobs, Tinit=1000, max_delay=500, device="cpu")
-    ratio = mc_mean/(np.arange(1,max_leadout+1)*mc_mean[0])
-    ratio_list.append(ratio)
-    for j in range(max_leadout):
-        if ratio[j]<=0.5:
-            Lhalf = j+1
-            break
-        else:
-            Lhalf = max_leadout
-        
-    Lhalf_list[i] = Lhalf
-    rho_list[i] = cor_calc(g,N,act_func, sigma_s, sigma_noise=sigma_n, trial=trial, Tobs= Tobs, Tinit=1000, device="cpu")
-    
-    print("******************************")
-    print(f"{i+1}/{n_data} was done")
-    print("******************************")
-    
-
-"""
-Judge whether numerically obtained Lhalf is within the theoretical scaling regime
-"""
-judge_list = np.zeros(len(rho_list))
-
-cnt=0
-for g, sigma_s, sigma_n, Lhalf in zip(g_list, sigma_s_list, sigma_n_list, Lhalf_list):
-    sigma_s_tilde = sigma_s*(N**0.25)
+for sigma_s_tilde in sigma_s_tilde_list:
     K = 0.1
     for _ in range(1000):
-        K = sigma_n**2 + sigma_s**2 + g**2 * (-1 + (4/np.pi)*np.arctan(np.sqrt(1+np.pi*K))) 
+        K = sigma_n**2 + g**2 * (-1 + (4/np.pi)*np.arctan(np.sqrt(1+np.pi*K))) 
     
-    alpha = np.arange(0.0, 1.01, 0.01)
     A = alpha*(sigma_s_tilde**2)/K
     B = g**2 / (1+0.5*np.pi*K)
+    
+    threshold = (K/np.sqrt((K-sigma_n**2)**2+sigma_s_tilde**4)) * np.sqrt(1-B**2)
+    threshold_list.append(threshold)
     
     mc_theory = np.zeros(len(alpha))
     for m in range(1,100):
         mc_theory += (-1)**(m-1) * (A**m) / (1-B**m)
-        
-    denominator = alpha[1:]*(sigma_s_tilde**2/K)/(1-B)
-    r = mc_theory[1:]/(denominator+ 1e-7)
-    diverged_alpha = alpha[np.sum(np.where(r[1:]-r[:-1]<0, 1, 0))+1]
-    diverged_L = diverged_alpha*np.sqrt(N)
     
-    # if Lhalf < diverged_L, then the plot is assumed to be within theory (denoted by "1")
-    judge_list[cnt] = 1*(Lhalf < diverged_L) 
-    cnt += 1
+    denominator = alpha*(sigma_s_tilde**2/K)/(1-B)
+    r = mc_theory/(denominator+ 1e-7)
+    diverge_judge = np.append(np.where(r[1:]-r[0:-1]<0, 1, np.nan),np.nan)
+    
+    r_list.append(r)
+    r_nondiverge_list.append(r*diverge_judge)
+    
 
+for i, sigma_s_tilde in enumerate(sigma_s_tilde_list):
+    sigma_s_tilde = round(sigma_s_tilde, 2)
+    plt.plot(alpha, r_list[i], color=str(1-(i+1)/10), linestyle="--")
+    plt.plot(alpha, r_nondiverge_list[i], label=r"$\tilde{\sigma}_s^2 =$"+f"{sigma_s_tilde}"+r"$^2$", color=str(1-(i+1)/10))
+    if int(threshold_list[i]/d_alpha) < len(alpha):
+        plt.scatter(threshold_list[i], r_list[i][int(threshold_list[i]/d_alpha)], marker="*",  s=150, color=str(1-(i+1)/10))
 
-# plot
-fig = plt.figure(figsize=(7,5))
-plt.scatter(rho_list[judge_list==1], Lhalf_list[judge_list==1], color="k") # a plot is black when it is within theory
-plt.scatter(rho_list[judge_list==0], Lhalf_list[judge_list==0], color="r") # a plot is red when it is beyond theory
-plt.xlabel("pair-wise average of "+r"$| \rho_{ij} |$",fontsize=17)
-plt.ylabel(r"$L_{\rm{half}}$",fontsize=20)
-plt.tick_params(labelsize=13) 
-plt.ylim(0,95)
-plt.yticks([0,10,20,30,40,50,60,70,80,90])
-plt.savefig("fig3.png")
+plt.xlabel(r"$\alpha$",fontsize=20)
+plt.ylabel(r"$r(\alpha)$",fontsize=20)
+plt.tick_params(labelsize=13)
+plt.ylim(0,1.1)    
+plt.legend(fontsize=10)
+plt.savefig("fig6.png")
--- fig4.py
+++ fig4.py
@@ -1,55 +1,97 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from utils.ipc import *
 from utils.util import *
+from scipy import special
+from scipy.stats import pearsonr
 
-g = 0.9
-input_intensity = 0.3
-noise_std = 0.0
-degree_list = [1,3,5,7,9]
-max_leadout = 50
+n_data = 100
+device = "cpu"
+
+N = 1000
+Tobs = 50000
+max_leadout = 100
 trial = 10
 
-ipc_list = np.zeros([trial, max_leadout, len(degree_list)])
+if device=="cpu":
+    act_func = np.tanh
+elif device=="mps":
+    act_func = torch.tanh
 
-for i in range(trial):  
-    ipc = calc_ipc(N,g,input_intensity,max_leadout,degree_list,Tobs,Tinit,act_func,noise_std=noise_std)
-    ipc_list[i,:,:] = ipc.copy()
-    print(f"trial {i+1}/{trial} was done")
+min_g = 0.2; max_g = 2.
+min_sigma_n = 0. ; max_sigma_n = 3.
+min_sigma_s = 0.1; max_sigma_s = 3.
 
-# top raw figure    
+Lhalf_list = np.zeros(n_data)
+rho_list = np.zeros(n_data)
+g_list = np.zeros(n_data)
+sigma_s_list = np.zeros(n_data)
+sigma_n_list = np.zeros(n_data)
+
+ratio_list = []
+
+for i in tqdm(range(n_data)):
+    g = min_g + (max_g-min_g)*np.random.rand()
+    sigma_n = min_sigma_n + (max_sigma_n-min_sigma_n)*np.random.rand()    
+    sigma_s = min_sigma_s + (max_sigma_s-min_sigma_s)*np.random.rand()    
+            
+    g_list[i] = g; sigma_s_list[i] = sigma_s; sigma_n_list[i] = sigma_n
+    
+    mc_mean, _ = mc_calc(g,N,act_func,max_leadout,sigma_s, sigma_noise=sigma_n, trial=trial, Tobs= Tobs, Tinit=1000, max_delay=500, device="cpu")
+    ratio = mc_mean/(np.arange(1,max_leadout+1)*mc_mean[0])
+    ratio_list.append(ratio)
+    for j in range(max_leadout):
+        if ratio[j]<=0.5:
+            Lhalf = j+1
+            break
+        else:
+            Lhalf = max_leadout
+        
+    Lhalf_list[i] = Lhalf
+    rho_list[i] = cor_calc(g,N,act_func, sigma_s, sigma_noise=sigma_n, trial=trial, Tobs= Tobs, Tinit=1000, device="cpu")
+    
+    print("******************************")
+    print(f"{i+1}/{n_data} was done")
+    print("******************************")
+    
+
+"""
+Judge whether numerically obtained Lhalf is within the theoretical scaling regime
+"""
+judge_list = np.zeros(len(rho_list))
+
+cnt=0
+for g, sigma_s, sigma_n, Lhalf in zip(g_list, sigma_s_list, sigma_n_list, Lhalf_list):
+    sigma_s_tilde = sigma_s*(N**0.25)
+    K = 0.1
+    for _ in range(1000):
+        K = sigma_n**2 + sigma_s**2 + g**2 * (-1 + (4/np.pi)*np.arctan(np.sqrt(1+np.pi*K))) 
+    
+    alpha = np.arange(0.0, 1.01, 0.01)
+    A = alpha*(sigma_s_tilde**2)/K
+    B = g**2 / (1+0.5*np.pi*K)
+    
+    mc_theory = np.zeros(len(alpha))
+    for m in range(1,100):
+        mc_theory += (-1)**(m-1) * (A**m) / (1-B**m)
+        
+    denominator = alpha[1:]*(sigma_s_tilde**2/K)/(1-B)
+    r = mc_theory[1:]/(denominator+ 1e-7)
+    diverged_alpha = alpha[np.sum(np.where(r[1:]-r[:-1]<0, 1, 0))+1]
+    diverged_L = diverged_alpha*np.sqrt(N)
+    
+    # if Lhalf < diverged_L, then the plot is assumed to be within theory (denoted by "1")
+    judge_list[cnt] = 1*(Lhalf < diverged_L) 
+    cnt += 1
+
+
+# plot
 fig = plt.figure(figsize=(7,5))
-hatch_list  = ['//', 'x', '||', '\\', "+", ".."]
-
-bottom = np.zeros([max_leadout])
-upper = np.mean(ipc_list[:,:,0], axis=0)
-
-for i in range(1, len(degree_list)+1):
-    plt.fill_between(np.arange(1,max_leadout+1), bottom, upper, label=f"D={degree_list[i-1]}", hatch=hatch_list[i-1])
-    bottom += np.mean(ipc_list[:,:,i-1], axis=0)
-    if i<len(degree_list):
-        upper += np.mean(ipc_list[:,:,i], axis=0)
-plt.plot(np.arange(1,max_leadout+1),np.arange(1,max_leadout+1), color="black",linestyle="--")
-plt.xticks([10,20, 30, 40, 50])
-plt.ylabel(r"$IPC_{total}$",fontsize=25)
-plt.tick_params(labelsize=15)
-plt.xlim(0,max_leadout)
-plt.ylim(0,max_leadout)
-plt.savefig("fig6top.png")
-
-# bottom raw figure  
-fig = plt.figure(figsize=(7,5))
-markers =["o", "x", "D", "s", "^", ">"]
-for i in range(0, len(degree_list)):
-    plt.plot(np.arange(1,max_leadout+1), np.mean(ipc_list[:,:,i], axis=0), label=f"D={degree_list[i]}", marker=markers[i], markersize=4)
-    plt.fill_between(np.arange(1,max_leadout+1), np.mean(ipc_list[:,:,i], axis=0)-np.std(ipc_list[:,:,i], axis=0),\
-                                                np.mean(ipc_list[:,:,i], axis=0)+np.std(ipc_list[:,:,i], axis=0), alpha=0.3)
-plt.xticks([10,20, 30, 40, 50])
-plt.xlabel(r"$L$",fontsize=25)
-plt.ylabel(r"$IPC_D$",fontsize=25)
-plt.tick_params(labelsize=15)
-plt.xlim(0,max_leadout)
-plt.ylim(0,50)
-plt.axhline(y=0, color="k", linestyle="--")
-plt.savefig("fig6bottom.png")
+plt.scatter(rho_list[judge_list==1], Lhalf_list[judge_list==1], color="k") # a plot is black when it is within theory
+plt.scatter(rho_list[judge_list==0], Lhalf_list[judge_list==0], color="r") # a plot is red when it is beyond theory
+plt.xlabel("pair-wise average of "+r"$| \rho_{ij} |$",fontsize=17)
+plt.ylabel(r"$L_{\rm{half}}$",fontsize=20)
+plt.tick_params(labelsize=13) 
+plt.ylim(0,95)
+plt.yticks([0,10,20,30,40,50,60,70,80,90])
+plt.savefig("fig3.png")
